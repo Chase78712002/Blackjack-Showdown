380175716d2f4a68b8f0cb2696c41c6627a834de
eyoa <yoaevelyn@gmail.com>
2021-07-28 19:53:08 -0400

    add bcrypt, setup server connection test route in users

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,37 @@
-// const { PORT = 5000 } = process.env;
+const PORT = process.env.PORT || 8080;
 
-// const app = require("./app");
-// const knex = require("./db/connection");
+const express = require('express');
+const app = express();
 
-// knex.migrate
-//   .latest()
-//   .then((migrations) => {
-//     console.log("migrations", migrations);
-//     app.listen(PORT, listener);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//     knex.destroy();
-//   });
 
-// function listener() {
-//   console.log(`Listening on Port ${PORT}!`);
-// }
+
+const knex = require('./db/connection');
+
+// //Routes
+const usersRoutes = require('./users/users.router');
+// // const apiRoutes = require('');
+
+const listener = () => {
+  console.log(`Listening on Port ${PORT}`);
+}
+
+
+// // Knex database
+
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log('migrations', migrations);
+    app.listen(PORT, listener);
+  })
+  .catch((error) => {
+    console.error(error);
+    knex.destroy();
+  });
+
+// resource routes
+app.use('/users', usersRoutes);
+// app.use('/api', apiRoutes(knex));
+
+
+app.listen(PORT, listener)
---
+++ server/src/users/users.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+module.exports = () => {
+  app.get("/", (req, res) => {
+    console.log("users route ping")
+    // res.send('Whoo');
+    return
+  })
+}
