2662714b983ed09efad45fe47dd915a751f00a69
eyoa <yoaevelyn@gmail.com>
2021-07-29 13:02:55 -0400

    add deck class

--- server/src/game/game.js
+++ server/src/game/game.js
@@ -1,9 +1,8 @@
 class Game {
-  constructor(id){
+  constructor(id) {
     this.id = id;
   }
 }
-
 
 class Card {
   constructor(name, suit, value) {
@@ -11,33 +10,37 @@ class Card {
     this.suit = suit;
     this.value = value;
   }
-
 }
 
+class Deck {
+  constructor() {
+    this.deck = [];
+    this.deckBuilder();
+  }
 
-const deckBuilder = () => {
-  const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
-  const suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
-  const deck = [];
+  get remaining() {
+    return this.deck.length;
+  }
 
-  for (let i = 0; i < suits.length; i++) {
-    for (let j = 0; j < values.length; j++) {
-      let numVal = 0;
-      let name = `${values[j]} of ${suits[i]}}`;
+  deckBuilder() {
+    const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+    const suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
 
-      if (values[j] == 'Ace') {
-        numVal = 11;
-      } else if (values[j].length > 3) {
-        numVal = 11;
-      } else {
-        numVal = values[j];
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < values.length; j++) {
+        let numVal = 0;
+        let name = `${values[j]} of ${suits[i]}}`;
+
+        if (values[j] == 'Ace') {
+          numVal = 11;
+        } else if (values[j].length > 3) {
+          numVal = 11;
+        } else {
+          numVal = values[j];
+        }
+
+        this.deck.push(new Card(name, suits[i], numVal));
       }
-
-      deck.push(new Card(name, suits[i], numVal));
     }
   }
-  return deck;
-};
-
-// const aceSpade = new Card('SA', 'Spades', 11);
-console.log(deckBuilder());
+}
