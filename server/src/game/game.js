0734639caa63c2485916b340563e66586dfc5074
eyoa <yoaevelyn@gmail.com>
2021-07-29 13:59:28 -0400

    add shuffle method

--- server/src/game/game.js
+++ server/src/game/game.js
@@ -22,6 +22,15 @@ class Deck {
     return this.deck.length;
   }
 
+  shuffle() {
+    for (let i = this.deck.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      const temp = this.deck[i];
+      this.deck[i] = this.deck[j];
+      this.deck[j] = temp;
+    }
+  }
+
   deckBuilder() {
     const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
     const suits = ['Diamonds', 'Clubs', 'Hearts', 'Spades'];
