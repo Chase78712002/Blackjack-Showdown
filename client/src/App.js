7f1085f4e27d7a3b80631efef4975a91484d73d6
Alvin Lin <alvinch.lin@gmail.com>
2021-07-29 20:57:39 -0700

    Add game start button for easier navigation in the root page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import LoginPage from './pages/LoginPage';
 import CreateUserPage from "./pages/CreateUserPage";
-
+import { Button } from './components/atoms/button/Button';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Game from './pages/Game/Game';
 
 function App() {
   return (
-    <>
-    <CreateUserPage />
-    <LoginPage />
-    </>
+    <Router>
+
+
+      <Switch>
+
+        <Route exact path="/">
+          <CreateUserPage />
+          <LoginPage />
+          <Link to="/game">
+          <Button variant='pixel'>Game starts</Button>
+          </Link>
+
+        </Route>
+
+        <Route path="/game">
+          <Game />
+        </Route>
+
+
+
+      </Switch>
+
+    </Router>
   );
 }
 
