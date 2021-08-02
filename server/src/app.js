907b0465a8a2146c67cb10f69090fbd6e953366b
eyoa <yoaevelyn@gmail.com>
2021-08-02 19:35:47 -0400

    adjust root path

--- server/src/app.js
+++ server/src/app.js
@@ -23,12 +23,11 @@ app.use(cors());
 app.use(express.json());
 
 //app.use(rootRouter);
-app.use(
-  '/',
-  express.static(
-    path.resolve(__dirname, '../../../client', 'build', 'index.html')
-  )
-);
+const root = path.join(__dirname, 'client', 'build');
+app.use(express.static(root));
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root });
+});
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
