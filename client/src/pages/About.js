c959ceeea59e39d2fb8ec3b52520a46a0e4a881f
eyoa <yoaevelyn@gmail.com>
2021-08-02 10:48:02 -0400

    add text drop shadow add social icons update descriptions adjusted spacing

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -10,16 +10,22 @@ export default function Aboutpage(props) {
           Back
         </Button>
       </Link>
+      <div className='top-spacer'></div>
       <h1>About Project</h1>
       <div className='text-block'>
-        This is a project done for the mintbean hackathon in July. We had one
-        week to create a fullstack card game.
+        A pixel-themed head to head blackjack game. It's a fullstack project
+        developed over one week for the Mintbean Hackathon July 2021.
       </div>
       <h1>Tech Stack</h1>
       <div className='text-block'>
-        BlackJack Showdown was devleoped using NodeJS and Express on the backend
+        BlackJack Showdown was devleoped using NodeJS, Express on the backend
         and React on the front end. We've used PostgreSQL for our database and
-        deployed on Heroku
+        deployed on Heroku.
+      </div>
+      <div className='text-block'>
+        Other libraries we've used are bcrypt for hashing passwords,jsonwebtoken
+        for login, knex to access the database, storybook for modular
+        developement and socket.io for real-time and capacity for multiplayer.
       </div>
       <div className='icons'>
         <img src='/img/tech/postgresql.png'></img>
@@ -32,24 +38,68 @@ export default function Aboutpage(props) {
         <div className='single'>
           <img src='/img/about/dice.png'></img>
           <div className='title'>Evelyn Yoa</div>
+
           <div className='desc'>
             A Jr. Dev with a background in VFX animation. Recently graduated
             from Lighthouse Labs fullstack web development bootcamp.
+          </div>
+          <div className='social-icon-container'>
+            <a href='https://github.com/eyoa'>
+              <img
+                className='social-icon'
+                src='/img/about/GitHub-Mark-64px.png'
+              ></img>
+            </a>
+            <a href='https://www.linkedin.com/in/evelyn-yoa/'>
+              <img
+                className='social-icon'
+                src='/img/about/linkedin_icon.png'
+              ></img>
+            </a>
           </div>
         </div>
         <div className='single'>
           <img src='/img/about/guy.png'></img>
           <div className='title'>Alvin Lin</div>
           <div className='desc'>A recent graduate from Lighthouse Labs.</div>
+          <div className='social-icon-container'>
+            <a href='https://github.com/Chase78712002'>
+              <img
+                className='social-icon'
+                src='/img/about/GitHub-Mark-64px.png'
+              ></img>
+            </a>
+            <a href='https://www.linkedin.com/in/alvin-lin-6933b39a/'>
+              <img
+                className='social-icon'
+                src='/img/about/linkedin_icon.png'
+              ></img>
+            </a>
+          </div>
         </div>
         <div className='single'>
           <img src='/img/about/dice.png'></img>
           <div className='title'>Audrey Plath</div>
-          <div className='desc'>A recent graduate from Lighthouse Labs.</div>
+          <div className='desc'>A recent graduate from name? bootcamp.</div>
+          <div className='social-icon-container'>
+            <a href='https://github.com/AudreyMargolis'>
+              <img
+                className='social-icon'
+                src='/img/about/GitHub-Mark-64px.png'
+              ></img>
+            </a>
+            <a href=''>
+              {/* <img
+              className='social-icon'
+              src='/img/about/linkedin_icon.png'
+            ></img> */}
+            </a>
+          </div>
         </div>
       </div>
 
       <br />
+      <h1>Credits</h1>
       <div className='text-block'>
         Thanks to Cazwolf for the pretty card images.{' '}
       </div>
