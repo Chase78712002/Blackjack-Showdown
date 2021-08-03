81d4c3a477c5b5fc042e45ccb2e654b98cf47886
eyoa <yoaevelyn@gmail.com>
2021-08-02 20:43:29 -0400

    update socket endpoint

--- client/src/utils/SocketProvider.js
+++ client/src/utils/SocketProvider.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const ENDPOINT = 'http://localhost:8080/';
+const ENDPOINT = process.env.REACT_APP_API_BASE_URL;
 export const socket = io.connect(ENDPOINT);
 export const SocketContext = React.createContext();
