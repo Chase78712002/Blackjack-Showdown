4fded8e30496d9c8ddcb638767fcb4cb9e8795a6
eyoa <yoaevelyn@gmail.com>
2021-08-25 12:03:57 -0400

    add card flipping animation

--- client/src/atoms/Card.js
+++ client/src/atoms/Card.js
@@ -3,14 +3,23 @@ import React from 'react';
 
 export default function Card({ name, imgUrl }) {
   return (
-    <div>
-      <div
-        className='card'
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-          backgroundSize: 'contain'
-        }}
-      ></div>
+    <div className='card'>
+      <div className='card-align'>
+        <div
+          className='card-back'
+          style={{
+            backgroundImage: `url("/img/PlayingCards/card-back1.png")`,
+            backgroundSize: 'contain'
+          }}
+        ></div>
+        <div
+          className='card-front'
+          style={{
+            backgroundImage: `url(${imgUrl})`,
+            backgroundSize: 'contain'
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
